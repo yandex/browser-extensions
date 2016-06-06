4c30001f9bfb6ca48da9b5c1d990e96838bebe4f
Nikita Begishev <begishev@77.88.16.66-red.dhcp.yndx.net>
2016-06-06 20:25:22 +0300

    MBROINFRA-146. Focus on report tab after test.

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -9,16 +9,23 @@ $(function () {
     test.addTestSet("Cookies", cookies_test);
     test.addTestSet("browserAction", browser_action_test);
 
-    test.runAll();
-    test.htmlReport().then(res => $body.append(res)).then(() => {
-        let $t = $(".test-set");
+    let test_tab_id;
 
-        $t.css('cursor', 'pointer').click(function() {
-            $(this).next().toggle('hidden');
+    new Promise(resolve => {
+        chrome.tabs.getCurrent(tab => {
+            test_tab_id = tab.id;
+            resolve();
         });
+    }).then(() => {
+        test.runAll();
+        test.htmlReport().then(res => $body.append(res)).then(() => {
+            let $t = $(".test-set");
 
-        chrome.windows.getCurrent((window) => {
-            chrome.windows.update(window.id, {focused: true})
+            $t.css('cursor', 'pointer').click(function() {
+                $(this).next().toggle('hidden');
+            });
+
+            chrome.tabs.update(test_tab_id, {active: true})
         });
     });
 });
