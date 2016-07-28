753cf85ee6d2428201d50d6e0e1f1382f7006756
Nikita Begishev <begishev@begishev-osx.local>
2016-07-28 22:45:07 +0400

    Flattened code for sessions test.

--- src/tests/sessions.js
+++ src/tests/sessions.js
@@ -21,39 +21,45 @@ var sessions_test = new TestSet()
             }, tab => {
                 chrome.tabs.onUpdated.addListener((tabID, info) => {
                     if (tab.id == tabID && info.status == 'complete') {
-                        chrome.tabs.remove(tab.id, () => {
-                            chrome.sessions.getRecentlyClosed(sessions => {
-                                let last = sessions.find(elem => elem.tab.url == test_url);
-                                if (last) {
-                                    chrome.sessions.restore(last.tab.sessionId, () => {
-                                        let resolved = false;
-                                        chrome.windows.getAll({populate: true}, windows => {
-                                            windows.reduce((chain_0, window_) => {
-                                                return chain_0.then(() => new Promise(resolve_0 => {
-                                                    window_.tabs.reduce((chain_1, tab) => {
-                                                        return chain_1.then(() => new Promise(resolve_1 => {
-                                                            if (tab.url == test_url) {
-                                                                chrome.tabs.remove(tab.id);
-                                                                resolve('');
-                                                                resolved = true;
-                                                            }
-                                                            resolve_1();
-                                                        }));
-                                                    }, Promise.resolve()).then(() => {
-                                                        resolve_0();
-                                                    });
-                                                }));
-                                            }, Promise.resolve()).then(() => {
-                                                if (!resolved) {
-                                                    reject("Restored tab not found");
-                                                }
-                                            });
-                                        });
-                                    })
-                                } else {
-                                    reject('Just closed tab not found in the recent closed')
-                                }
-                            })
+                        new Promise(next => {
+                            chrome.tabs.remove(tab.id, next)
+                        }).then(() => new Promise(next => {
+                            chrome.sessions.getRecentlyClosed(next);
+                        })).then(sessions => new Promise(next => {
+                            let last = sessions.find(elem => elem.tab.url == test_url);
+                            if (last) {
+                                next(last);
+                            } else {
+                                reject('Just closed tab not found in the recent closed')
+                            }
+                        })).then(last => new Promise(next => {
+                            chrome.sessions.restore(last.tab.sessionId, next);
+                        })).then(() => new Promise(next => {
+                            chrome.windows.getAll({populate: true}, next);
+                        })).then(windows => new Promise(next => {
+                            let resolved = false;
+                            windows.reduce((chain_0, window_) => {
+                                return chain_0.then(() => new Promise(resolve_0 => {
+                                    window_.tabs.reduce((chain_1, tab) => {
+                                        return chain_1.then(() => new Promise(resolve_1 => {
+                                            if (tab.url == test_url) {
+                                                chrome.tabs.remove(tab.id);
+                                                resolve('');
+                                                resolved = true;
+                                            }
+                                            resolve_1();
+                                        }));
+                                    }, Promise.resolve()).then(() => {
+                                        resolve_0();
+                                    });
+                                }));
+                            }, Promise.resolve()).then(() => {
+                                next(resolved);
+                            });
+                        })).then(resolved => {
+                            if (!resolved) {
+                                reject("Restored tab not found");
+                            }
                         });
                     }
                 });
